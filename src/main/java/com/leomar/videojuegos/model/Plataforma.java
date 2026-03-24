b4c269f5000c510e53b84237d422282c14754e05
Contreras Barroso <lcontrerasb@deloitte.com>
2026-03-24 14:28:24 -0300

    Se agregaron más plataformas a la lista de consolas

--- src/main/java/com/leomar/videojuegos/model/Plataforma.java
+++ src/main/java/com/leomar/videojuegos/model/Plataforma.java
@@ -1,16 +1,30 @@
 package com.leomar.videojuegos.model;
 
 public enum Plataforma {
-    PC,
-    PS1,
-    PS2,
-    PS3,
-    PS4,
-    PS5,
-    PSP,
-    XBOX_360,
-    NINTENDO_3DS,
-    WII,
-    GAME_BOY,
-    GAME_BOY_ADVANCE
+    PC("PC"),
+    PS1("PS1"),
+    PS2("PS2"),
+    PS3("PS3"),
+    PS4("PS4"),
+    PS5("PS5"),
+    PSP("PSP"),
+    XBOX_360("Xbox 360"),
+    NINTENDO_DS("Nintendo DS"),
+    NINTENDO_3DS("Nintendo 3DS"),
+    NINTENDO_SWITCH("Nintendo Switch"),
+    NINTENDO_SWITCH_2("Nintendo Switch 2"),
+    WII("Wii"),
+    GAME_BOY("Game Boy"),
+    GAME_BOY_ADVANCE("Game Boy Advance"),
+    SMARTPHONE("Smartphone");
+
+    private final String label;
+
+    Plataforma(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
